f4e587bcfb471e106d969398e67c7f012a7d09f5
Lime <elincraft2012@gmail.com>
2024-05-11 18:17:49 -0700

    no

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <p>SUSSY CUSTOMERS PAEG YASSSS</p>
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <p>SUSSY INVOICE PAGE BBUT IUQJHFCUXKSDJGUHVNF</p>
+}
